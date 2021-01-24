e25077a6905103092c47618e8ebb9b291f3aab6f
yanfeipeng <1178966910@qq.com>
2021-01-24 23:04:34 +0800

    first api.

--- src/main/java/api/FirstApi.java
+++ src/main/java/api/FirstApi.java
@@ -1,12 +1,21 @@
 package api;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/v1")
+/**
+ * todo @Controller 与@RestController的区别是什么？似乎@Controller会返回jsp视图
+ * 现象：@Controller 代码可以进来，但是报noHandler，@RestController正常
+ */
+
+@RestController
 public class FirstApi {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+
+    @RequestMapping(value = "/v1/fisrt-api", method = RequestMethod.GET, produces = "application/json")
+    public String helloWorld() {
+        return "hello world";
     }
 }
